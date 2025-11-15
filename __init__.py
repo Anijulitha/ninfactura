e27b5982924c3d7d49af8574c7da5d311de9d1b4
Anijulitha <anijuli2893@gmail.com>
2025-11-15 15:52:38 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import sys
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask, redirect, url_for
+from flask_login import current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
