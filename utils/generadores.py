caa208befe43701a3955ef2ebb64dacb35d6354f
Anijulitha <anijuli2893@gmail.com>
2025-11-25 22:27:25 +0100

    generadores.py

--- utils/generadores.py
+++ utils/generadores.py
@@ -29,38 +29,27 @@ def generar_pdf(factura):
     os.makedirs("facturas_templates/facturas/pdf", exist_ok=True)
     path = f"facturas_templates/facturas/pdf/factura_{factura.numero}.pdf"
 
-    # PDF MÍNIMO pero BONITO y 100% FUNCIONAL (sin caracteres raros)
-    texto = f"""
-    NINFACTURA - Factura {factura.numero}
-
-    Cliente: {factura.cliente_nombre}
-    NIF: {factura.cliente_nif or 'Pendiente'}
-
-    Concepto: Servicios de facturación Ninfactura
-    Base imponible: {factura.base_imponible:.2f} EUR
-    IVA 21%: {factura.iva:.2f} EUR
-    TOTAL: {factura.total:.2f} EUR
-
-    ¡Gracias por confiar en Ninfactura! 🚀💜
-    www.ninfactura.onrender.com
-    """
-
     contenido = f"""%PDF-1.4
 1 0 obj <</Type /Catalog /Pages 2 0 R>> endobj
 2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>> endobj
-3 0 obj <</Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Contents 4 0 R /Resources <</Font <</F1 5 0 R>>>>>> endobj
-4 0 obj <</Length 900>> stream
-BT /F1 24 Tf 100 800 Td (NINFACTURA - Factura {factura.numero}) Tj
-   100 750 Td (Cliente: {factura.cliente_nombre}) Tj
-   100 720 Td (NIF: {factura.cliente_nif or 'Pendiente'}) Tj
-   100 670 Td (Base: {factura.base_imponible:.2f} EUR  -  IVA: {factura.iva:.2f} EUR) Tj
-   /F1 36 Tf 100 600 Td (TOTAL: {factura.total:.2f} EUR) Tj
-   /F1 18 Tf 100 500 Td (Gracias por confiar en Ninfactura! (rocket)(purple_heart)) Tj
+3 0 obj <</Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Contents 4 0 R /Resources <</Font <</F1 5 0 R /F2 6 0 R>>>>>> endobj
+4 0 obj <</Length 1200>> stream
+BT /F2 38 Tf 170 790 Td (NINFACTURA) Tj
+   /F1 24 Tf 150 750 Td (Factura {factura.numero}) Tj
+   /F1 18 Tf 80 700 Td (Cliente: {factura.cliente_nombre}) Tj
+   80 670 Td (NIF: {factura.cliente_nif or 'Pendiente'}) Tj
+   80 620 Td (Concepto: Servicios Ninfactura) Tj
+   80 570 Td (Base imponible: {factura.base_imponible:.2f} EUR) Tj
+   80 540 Td (IVA 21%: {factura.iva:.2f} EUR) Tj
+   /F2 42 Tf 80 480 Td (TOTAL: {factura.total:.2f} EUR) Tj
+   /F1 18 Tf 80 400 Td (Gracias por confiar en Ninfactura! (rocket)(purple_heart)) Tj
+   80 360 Td (www.ninfactura.onrender.com) Tj
 ET
 endstream endobj
 5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Helvetica-Bold>> endobj
-xref 0 6 0000000000 65535 f 0000000010 00000 n 0000000074 00000 n 0000000125 00000 n 0000000285 00000 n 0000000700 00000 n 
-trailer <</Size 6 /Root 1 0 R>> startxref 1000 %%EOF""".encode('ascii', 'ignore')  # ← Esto ignora cualquier carácter raro
+6 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Helvetica>> endobj
+xref 0 7 0000000000 65535 f 0000000010 00000 n 0000000074 00000 n 0000000125 00000 n 0000000285 00000 n 0000000700 00000 n 0000000900 00000 n 
+trailer <</Size 7 /Root 1 0 R>> startxref 1300 %%EOF""".encode('utf-8')
 
     with open(path, "wb") as f:
         f.write(contenido)
