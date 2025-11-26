c30fac2bbaf390ee0c04c17f7672e2bba10efc6d
Anijulitha <anijuli2893@gmail.com>
2025-11-26 13:14:08 +0100

    auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from models.user import User
-from __init__ import db
+from ninfacturanuevo_app import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
